a0eb14802d258f8e830f39a77843b64644f5aeac
Brittany Larson <brittanylarson@Brittanys-Air.localdomain>
2024-01-17 16:45:20 -0700

    added bootstrap to make modal in my exercise component

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,52 @@
 import Exercise from './Exercise.jsx'
 import {useState} from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
 
   let [displayedProgramId, setDisplayedProgramId] = useState(-1)
  
-
+  // console.log(displayedProgramId)
   return (
-    <>
+   
+    < div className="app">
      <h1>Workouts of the Week</h1>
             <div>
                 <h2 className='day'>Monday</h2>
                 <h4>Glutes & Quads</h4>
                 <button onClick={() => setDisplayedProgramId(1)}>Exercises</button>
-                {displayedProgramId === 1 && <Exercise setDisplayedProgramId = {setDisplayedProgramId}/>}
+                {displayedProgramId === 1 && <Exercise displayedProgramId = {displayedProgramId} setDisplayedProgramId = {setDisplayedProgramId}/>}
             </div>
             <div>
                 <h2 className='day'>Tuesday</h2>
                 <h4>Back & Biceps</h4>
                 <button onClick={() => setDisplayedProgramId(2)}>Exercises</button>
-                {displayedProgramId === 2 && <Exercise setDisplayedProgramId = {setDisplayedProgramId}/>}
+                {displayedProgramId === 2 && <Exercise displayedProgramId = {displayedProgramId} setDisplayedProgramId = {setDisplayedProgramId}/>}
             </div>
             <div>
                 <h2 className='day'>Wednesday</h2>
                 <h4>Abs & Cardio</h4>
                 <button onClick = {() => setDisplayedProgramId(3)}>Exercises</button>
-                {displayedProgramId === 3 && <Exercise setDisplayedProgramId = {setDisplayedProgramId}/>}
+                {displayedProgramId === 3 && <Exercise displayedProgramId = {displayedProgramId} setDisplayedProgramId = {setDisplayedProgramId}/>}
             </div>
             <div>
                 <h2 className='day'>Thursday</h2>
                 <h4>Hamstrings & Glutes</h4>
                 <button onClick = {() => setDisplayedProgramId(4)}>Exercises</button>
-                {displayedProgramId === 4 && <Exercise setDisplayedProgramId = {setDisplayedProgramId}/>}
+                {displayedProgramId === 4 && <Exercise displayedProgramId = {displayedProgramId} setDisplayedProgramId = {setDisplayedProgramId}/>}
             </div>
             <div>
                 <h2 className='day'>Friday</h2>
                 <h4>Chest & Triceps</h4>
-                <button onClikc ={() => setDisplayedProgramId(5)}>Exercises</button>
-                {displayedProgramId === 5 && <Exercise setDisplayedProgramId = {setDisplayedProgramId}/>}
+                <button onClick ={() => setDisplayedProgramId(5)}>Exercises</button>
+                {displayedProgramId === 5 && <Exercise displayedProgramId = {displayedProgramId} setDisplayedProgramId = {setDisplayedProgramId}/>}
             </div>
      
-    </>
+    </div>
+
+    
+
   )
 }
 
--- src/Exercise.jsx
+++ src/Exercise.jsx
@@ -1,52 +1,62 @@
 import { useState } from 'react'
+// import './exercise.css'
+import {Button, Modal} from 'react-bootstrap';
 
 export default function Exercise(props){
+    const { setDisplayedProgramId } = props
 
-
+    const [show, setShow] = useState(true);
     return (
         <>
-            <div className="modal">
-
-                <div className="modal-box">
-                    <h2>Sumo Squats</h2>
+        <Modal 
+        size="xl"
+        show={show}
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+        >
+    
+            <Modal.Header closeButton onHide={() => setShow(false) & setDisplayedProgramId(-1)}>
+                <Modal.Title id="contained-modal-title-vcenter">Workout Program</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                
+                    <h3>Sumo Squats</h3>
                     <label>Reps:</label>
                     <input type="text" name ="reps"/><p>Goal: 8-10 Reps</p>
                     <p>Sets: 4</p>
-                    <button>Save</button>    
-                </div>
-                <div className="modal-box">
-                    <h2>Glute Bridge</h2>
+                    <Button>Save</Button>    
+                
+                    <h3>Glute Bridge</h3>
                     <label>Reps:</label>
                     <input type="text" name ="reps"/><p>Goal: 10 Reps</p>
                     <p>Sets: 4</p>
-                    <button>Save</button>    
-                </div>
-                <div className="modal-box">
-                    <h2>Leg Press</h2>
+                    <Button>Save</Button>    
+                
+                    <h3>Leg Press</h3>
                     <label>Reps:</label>
                     <input type="text" name ="reps"/><p>Goal: 9 Reps</p>
                     <p>Sets: 4</p>
-                    <button>Save</button>    
-                </div>
-                <div className="modal-box">
-                    <h2>Reverse Lunge</h2>
+                    <Button>Save</Button>    
+               
+                    <h3>Reverse Lunge</h3>
                     <label>Reps:</label>
                     <input type="text" name ="reps"/><p>Goal: 10 Reps</p>
                     <p>Sets: 4</p>
-                    <button>Save</button>    
-                </div>
-                <div className="modal-box">
-                    <h2>Box Jumps</h2>
+                    <Button>Save</Button>    
+              
+                    <h3>Box Jumps</h3>
                     <label>Reps:</label>
                     <input type="text" name ="reps"/><p>Goal: 8-10 Reps</p>
                     <p>Sets: 4</p>
-                    <button>Save</button>    
-                </div>
-                <br></br>
-                <button>Reset</button>
-                <button>Favorite</button>
-                {/* <div className="modal-background" onClick= {setDisplayedProgramId}></div> */}
-            </div>
+                    <Button>Save</Button>    
+                
+            </Modal.Body>
+            <Modal.Footer>
+                <Button>Reset</Button>
+                <Button>Favorite</Button>
+                {/* <div className="modal-background" onClick={setDisplayedProgramId}></div> */}
+            </Modal.Footer>
+        </Modal>
         </>
     )
 }
