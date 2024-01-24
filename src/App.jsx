6adf87d5853d2b55238e44820b90d24675546019
Brittany Larson <brittanylarson@Brittanys-Air.localdomain>
2024-01-24 11:42:39 -0700

    implemented flex on my cards

--- src/App.jsx
+++ src/App.jsx
@@ -30,16 +30,16 @@ function App() {
   return (
    
      <div className="app">
-     <h1>Workouts of the Week</h1>
-     <img src='https://static.vecteezy.com/system/resources/thumbnails/026/781/389/small_2x/gym-interior-background-of-dumbbells-on-rack-in-fitness-and-workout-room-photo.jpg' alt="Gym Image"/>
-            <div>
+     <h1 className="heading-title">Workouts of the Week</h1>
+     <img className="homepage-image"src='https://static.vecteezy.com/system/resources/thumbnails/026/781/389/small_2x/gym-interior-background-of-dumbbells-on-rack-in-fitness-and-workout-room-photo.jpg' alt="Gym Image"/>
+            <div className= "workout-cards">
               {programObjs.map((element, index) => {
-                return <Card className="card" style={{ width: '22rem'}} key={element.programId}>
+                return <Card className="card" style={{ width: '18rem'}} key={element.programId}>
                         <Card.Img variant='top'className="program-image" src={element.image} />
                         <Card.Body>
-                        <Card.Title>{weeks[index]}</Card.Title>
-                        <Card.Text>{element.name}</Card.Text>
-                        <Button variant="primary" onClick={() => setDisplayedProgramId(element.programId)}>Exercises</Button>
+                        <Card.Title className="card-content">{weeks[index]}</Card.Title>
+                        <Card.Text className="card-content">{element.name}
+                        <Button className="card-content" variant="primary" onClick={() => setDisplayedProgramId(element.programId)}>Exercises</Button>
                         {displayedProgramId === (element.programId) && 
                          
                          <Program
@@ -50,6 +50,7 @@ function App() {
                             setProgramObjs = {setProgramObjs}
                           />
                         }
+                        </Card.Text>
                         </Card.Body>
                   </Card>
               })
